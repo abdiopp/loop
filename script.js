73233ba9265d865e3bd8f67bc78d00d1cbb90c9a
Abdullah Murtaza <ginnieabdullah007@gmail.com>
2024-02-03 11:39:50 +0500

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+let i = 0;
+while (i < 5){
+    console.log(i);
+
+}
+
+while (i < 5){
+    console.log(i);
+    
+}
+
+while (b < 5){
+    console.log(i);
+    
+}
+while (a < 5){
+    console.log(i);
+    
+}
